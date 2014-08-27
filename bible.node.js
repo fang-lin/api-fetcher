dc991a06b22409fd95f1c5ba271464a39090a848
IsaacFang <isaac.fang@grapecity.com>
2014-08-27 14:54:34 +0800

    method

--- bible.node.js
+++ bible.node.js
@@ -15,15 +15,11 @@ versions.forEach(function (version) {
     var result = {};
 
     books.forEach(function (book, bookIndex, books) {
-
         chapters.forEach(function (chapter, chapterIndex, chapters) {
-
             var trees = util.readJson('api/bible/tree/' + version + '/' + book + '/' + chapter + '.json');
 
             trees.forEach(function (tree, treeIndex, trees) {
-
                 tree.nodes.forEach(function (node, nodeIndex, nodes) {
-
                     var id = node.id;
 
                     util.fetchJson(config.apiBaseUrl + 'bible/node/' + version + '/' + id)
@@ -43,33 +39,6 @@ versions.forEach(function (version) {
         });
     });
 });
-
-//Object.keys(seeds).forEach(function (filename) {
-//    var slices = filename.split('/'),
-//        mainVersion = slices[0];
-//
-//    versions[mainVersion].forEach(function (subVersion) {
-//
-//        var result = {};
-//
-//        seeds[filename].forEach(function (tree, treeIndex, trees) {
-//
-//            util.treeToList(tree['tree']).forEach(function (id, nodeIndex, nodes) {
-//
-//                util.fetchJson(config.apiBaseUrl + 'bible/node/' + mainVersion + '/' + id)
-//                    .then(function (chunk) {
-//                        result[id] = JSON.parse(chunk);
-//
-//                        if (treeIndex + 1 === trees.length &&
-//                            nodeIndex + 1 === nodes.length) {
-//
-//                            util.writeJsonToFiles('api/bible/node/' + mainVersion, JSON.stringify(result));
-//                        }
-//                    });
-//            });
-//        });
-//    });
-//});
 
 
 
